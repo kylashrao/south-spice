0c1ecc39127de23025fc79059bfb088b70252899
Kailash Rao S <kylash.rao@gmail.com>
2026-06-07 23:41:05 +0530

    Change CSS import path to use alias
    Updated CSS import path to use Vite config alias.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import './index.css';
+import '@/index.css'; // If using your vite.config path alias
 
 createRoot(document.getElementById("root")!).render(<App />);
