b1117e53013051d81c63bf96cb7047c1bc591dbe
Kailash Rao S <kylash.rao@gmail.com>
2026-06-09 00:03:50 +0530

    Replace CookedPage with CookedHistory route

--- src/App.tsx
+++ src/App.tsx
@@ -119,8 +119,7 @@ function App() {
           <Route path="/shopping-list" component={ShoppingList} />
           <Route path="/pantry" component={Pantry} />
           <Route path="/meal-plan" component={MealPlan} />
-          <Route path="/cooked-history" component={CookedPage} />
-          {/* <Route path="/cooked-history" component={CookedHistory} /> */}
+          <Route path="/cooked-history" component={CookedHistory} />
           <Route path="/newsletter" component={Newsletter} />
           <Route path="/search" component={SearchPage} />
 
